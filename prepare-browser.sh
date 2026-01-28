ee1eb1439bd1aa452303404f361a7acc9bfba894
Victor Derks <vbaderks@gmail.com>
2026-01-28 17:33:27 +0100

    Process code review comments

--- prepare-browser.sh
+++ prepare-browser.sh
@@ -2,7 +2,11 @@
 # SPDX-FileCopyrightText: © 2026 Team CharLS
 # SPDX-License-Identifier: BSD-3-Clause
 
+# When running a local http server (for example python3 -m http.server), the server
+# cannot access files outside the root folder. Therefore copy the built files to
+# the test/browser/dist folder.
+
 cp ./dist/charlsjs.js ./test/browser/dist/
 cp ./dist/charlsjs.wasm ./test/browser/dist/
-cp ./src/JpegLSDecoder.js ./test/browser/dist/
-cp ./src/JpegLSEncoder.js ./test/browser/dist/
+cp ./dist/JpegLSDecoder.js ./test/browser/dist/
+cp ./dist/JpegLSEncoder.js ./test/browser/dist/
